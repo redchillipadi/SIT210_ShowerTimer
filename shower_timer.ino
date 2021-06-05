d650a6b87ef0480dc9bff92609f7d7b8a1f2e8a7
Redchillipadi <agrigo2001@yahoo.com.au>
2021-06-05 10:03:10 +1000

    Updated comments

--- shower_timer.ino
+++ shower_timer.ino
@@ -1,226 +1,236 @@
 #include <avr/sleep.h>
 
-// The HM-10 Bluetooth 4.0 module is connected with
-// VCC to 5V
-// GND to Ground
-// TXD connecting to RXD on the ATmega328P,
-// RXD connecting to TXD (pin 1 on the ATmega328P), and also via a 220 Ohm resistor to INT1 (pin 3) to enable wake on Bluetooth.
+// PIN CONNECTIONS
+const int FLOW_PIN = 2;             // Signals from the Flow sensor are monitored on pin D2 (Int 0)
+const int BLUETOOTH_PIN = 3;        // Signals from the Bluetooth module are monitored on pin D3 (Int 1)
+const int SOLENOID_INPUT_A = 12;    // Control for the motor driver for the solenoid are from pins D12 and D13
+const int SOLENOID_INPUT_B = 13;    // Setting one high and the other low applies a positive or negative voltage to the solenoid
+const int LM335_PIN = A0;           // The output from the temperature sensor is connected to A0
+const int VOLTAGE_PIN = A1;         // Monitor the battery voltage which is connected to A1
 
-const int BLUETOOTH_PIN = 3;  // The pin which is monitored for Bluetooth interrupts
+// GLOBAL VARIABLES FOR STORING STATE
+volatile int flow_count;      // Stores the number of water flow signals received. Each is 1/450 L.
+bool solenoid_open;           // Record whether the solenoid is currently open
+unsigned long sensorStart;    // The time the sensors were last read (used to determine when to read again)
+unsigned long showerStart;    // The time the shower was started (used to determine when to close the solenoid)
+unsigned long watchdogStart;  // The time the last control signal was received (used to determine when to sleep)
 
-// Use hardware serial for Bluetooth communication, which is better than Software Serial and altSoftwareSerial
-// as it allows simultaneous bidirectional communication and has lower interrupt latency.
+// CONSTANTS
+const float REFERENCE_VOLTAGE = 2.49;                             // The external reference voltage chip is calibrated to 2.49V
+const float ADC_RESOLUTION = 1024.0;                              // The number of gradations of the ADC.
+const float TEMP_VOLTAGE_DIVIDER = 200000.0/(200000.0+100000.0);  // The resistor divider which reduces the voltage from the LM335
+const float VOLT_VOLTAGE_DIVIDER = 7500.0/(7500.0+30000.0);       // The resistor divider which reduces the voltage from the battery
+const float PULSES_PER_LITRE = 450.0;                             // The number of pulses from the flow sensor per litre of flow
+const float ABSOLUTE_ZERO = -273.15;                              // Offset to convert from Kelvin to Celsius
+const unsigned long SOLENOID_PULSE_DURATION = 20;                 // Change the solenoid position with a 20 millisecond pulse
+const unsigned long SENSOR_READ_INTERVAL = 1000;                  // Read the sensors every second (value in milliseconds)
+const unsigned long SHOWER_DURATION   = 240000;                   // The shower will lock 4 minutes after being started (value in milliseconds)
+const unsigned long WATCHDOG_DURATION = 10000;                    // If the solenoid is closed, the device will sleep after 10 seconds of inactivity (value in milliseconds)
 
-// On the ATmega328P, attachInterrupt only works on pins 2 or 3
-const int FLOW_PIN = 2;       // The pin to monitor for water flow signals (each represents 1/450 L)
-volatile int flow_count;      // Store the number of signals received in the last second
-
-// The LM9110 is connected with
-// Solenoid Input A to pin 12 on the ATmega328P.
-// Solenoid Input B to pin 13 on the ATmega328P.
-
-// The solenoid is connected with the lead closest to the water source to Pin 1, and the lead furthest away to Pin 4.
-const int SOLENOID_INPUT_A = 12;
-const int SOLENOID_INPUT_B = 13;
-bool solenoid_open;
-int solenoid_count;
-
-const int LM335_PIN = A0;
-// The GPIO output is divided by a 2.2k resistor and the chip,
-// and calibrated using a trimpot to the adj. leg to account for variations in supply voltage
-// Another possibility would be to use a reference voltage chip
-
-const int VOLTAGE_PIN = A1;
-// The input voltage is divided by a 30k and 7k5 resistor
-// This gives 2.37V for 12V  and 1.00 V for 5V
-// At 12V it draws 0.3 mA
-const float REFERENCE_VOLTAGE = 2.5;
-const float ADC_RESOLUTION = 1024.0;
-const float TEMP_VOLTAGE_DIVIDER = 2200.0/(2200.0+820.0);
-
-const long SHOWER_DURATION   = 240000; // 4 minutes in milliseconds
-const long WATCHDOG_DURATION = 300000; // 5 minutes in milliseconds
-unsigned long showerStart;
-unsigned long watchdogStart;
-
-// With the Arduino Uno, AltSoftSerial requires D8 connected to TXD, and D9 connected to RXD on the bluetooth module
-// but using Hardware Serial requires D0 connected to TXD and D1 connected to RXD on the bluetooth module
-
+// setup - Set up the ATmega328P, with global variables and pins in a known state
+// Params: None
+// Returns: Nothing
+//
+// The solenoid will be closed, and the timers reset so the sensors are ready to be read,
+// and the system will go to sleep after 5 minutes of inactivity
 void setup() {
-  // TODO: The HM-10 can support 115200, and the Serial library probably can't support the maximum of 230400
-  // according to http://www.martyncurrey.com/hm-10-bluetooth-4ble-modules/
-  Serial.begin(9600);
-  // Commands are sent at 9600 baud regardless of the baud rate.
-  // The rate is set by sending AT+BAUDx (where x=0 for 9600, 4 for 115200 or 8 for 230400)
-  // and the device should return AT+Set:x
-  // Then flush the command, change to the correct speed and discard any received junk in the buffer
-  // But something is not quite right as the phone only prints junk
+  Serial.begin(9600); // For transferring readings every second, the default baud rate is sufficient
 
-  solenoid_count = 0;
-  solenoid_open = false;
-  digitalWrite(SOLENOID_INPUT_A, LOW);
+  solenoid_open = false;                  // On boot, ensure the solenoid is closed
+  digitalWrite(SOLENOID_INPUT_A, LOW);    // Put the solenoid controller pins into a known state
   digitalWrite(SOLENOID_INPUT_B, LOW);
-  pinMode(SOLENOID_INPUT_A, OUTPUT);
+  pinMode(SOLENOID_INPUT_A, OUTPUT);      // and have them ready for output
   pinMode(SOLENOID_INPUT_B, OUTPUT);
-  writeSolenoid(solenoid_open);
+  writeSolenoid(solenoid_open);           // Send a signal to the solenoid so it also is in a known state
 
-  pinMode(LM335_PIN, INPUT);
+  // Setting analogReference to EXTERNAL must be called before reading from the analog pins to prevent a short circuit
+  pinMode(LM335_PIN, INPUT);              // Set the required analog pins into INPUT mode
   pinMode(VOLTAGE_PIN, INPUT);
-  analogReference(EXTERNAL);
+  analogReference(EXTERNAL);              // and use the external voltage reference
 
-  flow_count = 0;
-  pinMode(FLOW_PIN, INPUT);
+  flow_count = 0;                         // On boot, the measured flow is 0L
+  pinMode(FLOW_PIN, INPUT);               // Use the pin for input, and register the interrupt for handling flow signals.
   attachInterrupt(digitalPinToInterrupt(FLOW_PIN), measureWaterFlow, RISING);
 
-  pinMode(BLUETOOTH_PIN, INPUT);
+  pinMode(BLUETOOTH_PIN, INPUT);          // Similarly, set the Bluetooth monitor pin to input and ensure it is low.
   digitalWrite(BLUETOOTH_PIN, LOW);
 
-  showerStart = millis() - SHOWER_DURATION;
-  resetWatchdog();
+  sensorStart = millis() - SENSOR_READ_INTERVAL;  // The sensors are ready to be read again now
+  showerStart = millis() - SHOWER_DURATION;       // Set the shower start time to 4 minutes before now, which corresponds to the state where the solenoid is closed.
+  watchdogStart = millis() - WATCHDOG_DURATION;   // Set the watchdog start time so that the system will go to sleep until it is woken by the controller.
 }
 
-// Send a 20 ms pulse to SOLENOID_INPUT_B to close, or SOLENOID_INPUT_A to open
+
+// writeSolenoid - Set the solenoid valve to the desired state
+// Params: Open - a boolean that represents whether the solenoid should be opened (otherwise it is closed)
+// Returns: Nothing
+//
+// Send a pulse to SOLENOID_INPUT_B to close, or SOLENOID_INPUT_A to open
 // Then leave both pins LOW to prevent drawing power while the latched solenoid maintains the open or closed state
 void writeSolenoid(bool Open)
 {
   digitalWrite(SOLENOID_INPUT_A, Open ? LOW : HIGH);
   digitalWrite(SOLENOID_INPUT_B, Open ? HIGH : LOW);
-  delay(20);
+
+  // Wait to allow time for the solenoid valve to change its position
+  float startTime = millis();
+  while ((millis() - startTime) < SOLENOID_PULSE_DURATION);
+  
   digitalWrite(SOLENOID_INPUT_A, LOW);
   digitalWrite(SOLENOID_INPUT_B, LOW);
 }
 
+
+// measureWaterFlow - Interrupt handler to record another signal from the flow sensor
+// Params: None
+// Returns: None
+//
+// The interrupt handler is called each time 1/450 L of flow is detected.
+// Increment the counter each time. This will be read and reset in the main loop each second.
 void measureWaterFlow()
 {
   flow_count++;
 }
 
-// Call this once per second to get the flow rate in L/sec
+
+// readFlowRate - Get the volume of water which has been measured since the last call
+// Params: None
+// Returns: None
+//
+// Read the current value of the flow count variable, and convert it to Litres
+// If this is called every second, the reading is in L/sec.
 float readFlowRate()
 {
-  float volume = (float)flow_count / 450.0;
+  float volume = (float)flow_count / PULSES_PER_LITRE;
   flow_count = 0;
   return volume;
 }
 
-// A count of 0 = 0 Kelvin = -273.15 C.
-// The voltage across the sensor increases by 0.01V/C, and should be 2.9815V at 25 C = 298.15 K
-// This is passed through a voltage divider so that only half the value is read by the arduino
-// This means the arduino pin should receive 1.49075 V at 298.15 K
-// Using the reference voltage of 2.94V (+/- 4%), which is equivalent to a count of 1024
-// this gives a nominal 0.002431642 Volts per count at the arduino pin, and so
-// 25C = 298.15K = 2.9815 V at the sensor = 1.49075V at the ardino pin = a count of 613.06107151
-// Converting backwards, a count of 613.06107151 = 298.15 K = 0.4863284 K/count
+
+// readTemperature - Return the current temperature in degrees Celsius
+// Params: None
+// Returns: the calculated temperature
+//
+// The sensor outputs 10 mV/K, and can be calibrated using the 10k potentiometer
+// This has been divided by a resistor divider to ensure that
+// is never above 2.5V that would maximise the analog to digital converter count
+// within the working water temperature range 0 to 65 C.
 float readTemperature()
 {
   float count = analogRead(LM335_PIN);
-  float temperature = count * 100.0 / TEMP_VOLTAGE_DIVIDER * REFERENCE_VOLTAGE / ADC_RESOLUTION - 273.15;
+  float temperature = count * 100.0 / TEMP_VOLTAGE_DIVIDER * REFERENCE_VOLTAGE / ADC_RESOLUTION + ABSOLUTE_ZERO;
   return temperature;
 }
 
-// The input to the voltage pin is divided using a voltage divider setup of a 30k and 7k5 resistor, which divides the input voltage by 5
-// When the supply voltage is 12.45 V this will give the reference voltage of 2.49 V (+/- 4%), which is equivalent to a count of 1024
-// and a count of 0 represents 0V, giving a nominal 0.012158203 volts per count
+
+// readVoltage - Return the battery voltage in Volts
+// Params: None
+// Returns: the calculated voltage
+//
+// The battery voltage is divided between a 30k and 7k5 resistor, reducing it to 20% of the original value
+// so with the 2.5V reference chip it can read up to a maximum of 12.5V
 float readVoltage()
 {
   float count = analogRead(VOLTAGE_PIN);
-  float voltage = count * 5.0 * REFERENCE_VOLTAGE / ADC_RESOLUTION;
+  float voltage = count / VOLT_VOLTAGE_DIVIDER * REFERENCE_VOLTAGE / ADC_RESOLUTION;
   return voltage;
 }
 
+
+// loop - Perform regular processing while the ATmega328P is awake
+// Params: None
+// Returns: Nothing
+//
+// There are a number of tasks:
+// - Read the sensors every second and send them to the Raspberry Pi via Bluetooth
+// - Respond to commands from the raspberry pi
+// - Turn the solenoid off after the shower has lasted 4 minutes
+// - and go to sleep after 5 minutes
 void loop() {
-  float temperature = readTemperature();
-  float flow = readFlowRate();
-  float voltage = readVoltage();
-
-    // If we don't use the external reference, correct for the measured voltage
-  //temperature = ((temperature + 273.15) * voltage / 5.0) - 273.15;
-
   unsigned long currentTime = millis();
-  if (solenoid_open && (currentTime - showerStart) > SHOWER_DURATION)
+
+  // Check if it is time to turn off the shower or go to sleep for inactivity
+  // The method of comparison used will still function correctly when millis overflows once every 50 days
+  if (solenoid_open && (currentTime - showerStart) >= SHOWER_DURATION)
   {
     solenoid_open = false;
     writeSolenoid(solenoid_open);
+    watchdogStart = currentTime;
   }
-  if ((currentTime - watchdogStart) > WATCHDOG_DURATION)
+  if (!solenoid_open && (currentTime - watchdogStart) >= WATCHDOG_DURATION)
   {
     goToSleep();
   }
-  
+
+  // Check for the open command from the Raspberry Pi
   if (Serial.available()) {
     char c = Serial.read();
-    if (c == 'O') {
-      // Open the solenoid and start countdown
+    if (c == 'o') {
       solenoid_open = true;
       writeSolenoid(solenoid_open);
-      setShowerCountdown();
-      resetWatchdog();
-    } else if (c == 'C') {
-      // Close the solenoid
-      solenoid_open = false;
-      writeSolenoid(solenoid_open);
-    } else if (c == 'W') {
-      // Wake from sleep command
-      // wakeOnBluetooth has already woken the arduino up
-      // and called resetWatchdog so there is nothing left to do here
-    } else if (c == 'S') {
-      // Go to sleep now for testing purposes
-      goToSleep();
+      showerStart = currentTime;
+      watchdogStart = currentTime;        
     }
-    // Invalid entries are silently ignored
   } 
 
-  char buffer[57];
-  char tempString[7];
-  char flowString[8];
-  char voltageString[7];
-  // Using this floating point adds about 3k program storage space
-  // and buffer space. Rewrite this to use a better protocol
-  dtostrf(temperature, 3, 1, tempString);
-  dtostrf(flow, 2, 3, flowString);
-  dtostrf(voltage, 2, 2, voltageString);
-  
-  sprintf(buffer, "Temp %s C | Flow %s L/s | Volts %s V | Solenoid %s", tempString, flowString, voltageString, solenoid_open ? "Open" : "Closed");
-  Serial.println(buffer);
 
-  delay(1000);
+  // Check if it is time to update the sensor values
+  if ((currentTime - sensorStart) >= SENSOR_READ_INTERVAL) {
+    // Read all the sensors
+    float temperature = readTemperature();
+    float flow = readFlowRate();
+    float voltage = readVoltage();
+
+    // Send the current readings to the raspberry pi
+    char buffer[57];
+    char tempString[7];
+    char flowString[8];
+    char voltageString[7];
+    // Convert each floating point reading into a string
+    dtostrf(temperature, 3, 1, tempString);   // The temperature is reported to 1 decimal place
+    dtostrf(flow, 2, 3, flowString);          // The flow rate is reported to 3 decimal places
+    dtostrf(voltage, 2, 2, voltageString);    // The voltage is reported to 2 decimal places
+    // Create and send the string via bluetooth
+    sprintf(buffer, "Temp %s C | Flow %s L/s | Volts %s V | Solenoid %s", tempString, flowString, voltageString, solenoid_open ? "Open" : "Closed");
+    Serial.println(buffer);
+
+    sensorStart = currentTime;
+  }
 }
 
-void setShowerCountdown()
-{
-  showerStart = millis();
-}
 
-void resetWatchdog()
-{
-  watchdogStart = millis();
-}
-
-// Wake on bluetooth needs connecting pin 2 (RXD) to pin 5 (INT 1)
-
-// The system draws 24.5 mA when connected with only the atmega328p chip. Using the arduino uno draws 32 mA
-// The system spikes to about 140 mA on the meter during solenoid use but as it is a brief pulse I think this is inaccurate. It should use about 800 mA according to valves direct.
-// According to https://rightbattery.com/118-1-5v-aa-duracell-alkaline-battery-tests/ I can expect each battery to give 2348 mAh at 100 mA load (this is longer than at higher loads)
-// So 8 batteries should provide 12 V initially, and drop to 8 V over 767 hours, so the batteries should need replacement about once a month.
-
-// Duracell also has rechargeable 2500 mAh batteries, which perhaps only provide 1.2V like other rechargeable batteries.
-// If so they would output 9.6 v when fresh, dropping to 7.68v over 816 hours, which is 34 days.
-// This would allow two sets to be interchanged for uninterrupted operation once a month
-
+// goToSleep - Put the system to sleep
+// Params: None
+// Returns: Nothing
+//
+// Called after a period of inactivity to put the system to sleep
+// It enables interrupt handlers so that it will be woken again if flow or bluetooth communication is detected
+// and resets the watchdog timer on waking up so that it will not be put back to sleep immediately by the main loop
 void goToSleep()
 {
-  // Enable sleep mode
-  sleep_enable();
+  sleep_enable();                       // Enable sleep mode
+  // Remove the usual interrupt handler from the flow pin
   detachInterrupt(digitalPinToInterrupt(FLOW_PIN));
+  // Then assign it to the wakeOnFlow handler
+  // The pin is LOW when inactive and rises when a signal is detected.
   attachInterrupt(digitalPinToInterrupt(FLOW_PIN), wakeOnFlow, RISING);
+  // Also assign the bluetooth monitoring pin to trigger wakeOnBluetooth
+  // This pin will be HIGH when inactive and becomes LOW during transmission
   attachInterrupt(digitalPinToInterrupt(BLUETOOTH_PIN), wakeOnBluetooth, LOW);
-  sei();
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_cpu();
-  sleep_disable();
-  resetWatchdog();    // After wake up, reset the watchdog
+  sei();                                // Ensure interrupts are enabled otherwise it will not be possible to wake again
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);  // This sleep mode uses the lowest current, but preserves the register values
+  sleep_cpu();                          // Put the ATmega328P to sleep
+  sleep_disable();                      // After waking up, disable the sleep mode
+  watchdogStart = millis();             // and restart counting the period of inactivity before it sleeps again
 }
 
+
+// wakeOnFlow - Called when flow is detected while the system is asleep
+// Params: None
+// Returns: Nothing
+//
+// First remove the wake interrupt handlers and restore the usual flow measurement interrupt
+// Then record the water flow
 void wakeOnFlow() {
   detachInterrupt(digitalPinToInterrupt(FLOW_PIN));
   detachInterrupt(digitalPinToInterrupt(BLUETOOTH_PIN));
@@ -228,6 +238,13 @@ void wakeOnFlow() {
   flow_count=1;
 }
 
+
+// wakeOnBluetooth - Called when bluetooth transmission is detected while the system is asleep
+// Params: None
+// Returns: Nothing
+//
+// First remove the wake interrupt handlers and restore the usual flow measurement interrupt
+// The received input will be in the buffer and will be handled by the main loop now the system is awake again
 void wakeOnBluetooth() {
   detachInterrupt(digitalPinToInterrupt(FLOW_PIN));
   detachInterrupt(digitalPinToInterrupt(BLUETOOTH_PIN));
